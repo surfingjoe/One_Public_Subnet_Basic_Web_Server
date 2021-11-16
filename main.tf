573bb34b8db23b1f18227bbf888b31890b648471
surfingjoe <surfingjoe2@gmail.com>
2021-11-16 06:35:31 -0800

    add terraform provider info

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
 # --------- Declare the AWS Region -------------------------------
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
